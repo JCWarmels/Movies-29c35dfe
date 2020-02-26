fa98e618f9298ffd2baebc0c14fe7e057a5fee69
JCWarmels <jesse.warmels@student.rocva.nl>
2020-02-26 09:48:44 +0100

    Uitwerking van de exercise

---
+++ movies.sql
@@ -0,0 +1,9 @@
+
+truncate films;
+INSERT INTO films (titel, duur_in_min, datum_van_uitkomst, land_van_uitkomst, omschrijving, trailer_id_youtube)
+VALUES  ('Primal', 97, '2019-11-8', 'US', 'Frank Walsh jaagt op exotische dieren die hij naar de VS verscheept. Wanneer hij een zeldzame witte jaguar vindt, denkt hij rijk te worden. Hij neemt het dier mee op het vrachtschip waarmee ook een moordenaar en andere dodelijke dieren reizen. Als de moordenaar weet uit te breken en de roofdieren bevrijdt, moet Frank vechten voor zijn leven.', 'xpxT6lWQDA4'),
+		('The Sea Inside', 126, '2004-09-3', 'ES', 'The factual story of Spaniard Ramon Sampedro, who fought a thirty-year campaign in favor of euthanasia and his own right to die.', 'dVRnG1MddAM'),
+        ('Trainspotting', 93, '1996-02-23', 'UK', 'Renton, deeply immersed in the Edinburgh drug scene, tries to clean up and get out, despite the allure of the drugs and influence of friends.', '8LuxOYIpu-I'),
+        ('In The Mood for Love', 98, '2000-05-20', 'FR', 'Two neighbors, a woman and a man, form a strong bond after both suspect extramarital activities of their spouses. However, they agree to keep their bond platonic so as not to commit similar wrongs.', 'dWVDZ98AFhI'),
+        ('The Lion King', 118, '2019-07-17', 'US', 'The Lion King is een muzikale fantasyfilm uit 2019, geregisseerd door Jon Favreau en geproduceerd door Walt Disney Pictures. Het is een realistische computer-geanimeerde remake van Disneys animatieklassieker uit 1994. In de bioscopen werd de film uitgezonden in 3D of 4D', '7TavVZMewpY');
+
